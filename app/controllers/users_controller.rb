656e562635aa20309a1d3a488ef5ceac0b169128
sndesai92 <sndesai@ncsu.edu>
2016-09-25 22:17:49 -0400

    Edit user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,27 @@ class UsersController < ApplicationController
   def index
   end
   def edit
-
+  @user = User.find(params[:id])
   end
 
   def show
   end
 
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Updated your profile successfully.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password_digest)
+  end
 end
