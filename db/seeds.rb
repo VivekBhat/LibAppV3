6fb20b6904a57a6f0bcb5e93eb04de4402a95477
vivek <bhatvivek93@gmail.com>
2016-09-27 21:29:10 +0000

    seeds added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(
-    name: "BAAP",
+    name: "SuperAdmin",
     email: "Admin@example.com",
     password_digest: 'pass',
     ornot: '1'
