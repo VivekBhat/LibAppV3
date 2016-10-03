c245d2b38ee42d8217f614fec5ab223a05d7d398
Jainpriyal <jainpriyal168@gmail.com>
2016-10-03 01:17:25 -0400

    correcting room number search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,7 +1,7 @@
 class Search < ApplicationRecord
   def search_rooms
     rooms = Room.all
-    rooms = rooms.where(["roomnumber LIKE ?", roomnumber ]) if roomnumber.present?
+    rooms = rooms.where(["roomnumber = ?", roomnumber ]) if roomnumber.present?
     rooms = rooms.where(["building LIKE ?", building ]) if building.present?
     rooms = rooms.where(["status LIKE ?", status]) if status.present?
     rooms = rooms.where(["size LIKE ?", size]) if size.present?
